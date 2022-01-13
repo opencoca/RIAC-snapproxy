b03cff19844c5549afc8702db270b447e094d7a8
OpenCo <admin@openco.ca>
2022-01-13 15:15:25 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 args="$@"
 
-args="$@ --port 80"
+args="$@ $args --port 80"
 
-args="$@ -u https://snap.berkeley.edu"
+args="$@ $args -u https://snap.berkeley.edu"
 
-args="$@ -z -l -R"
+args="$@ $args -z -l -R"
 
-args="$@  -o https://snap.robotinacan.com"
+args="$@  $args -o https://snap.robotinacan.com"
 
 json-caching-proxy $args
